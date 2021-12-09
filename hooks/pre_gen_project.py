62fe263985e31252b1329a4afb3ecc5b4697872c
Lucas Oliveira <lucaso@agap2.pt>
2021-12-09 11:49:15 +0000

    Update Version Readme

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -3,6 +3,3 @@ import re
 
 
 dns_name = re.sub(r'([a-z0-9])([A-Z])',r'\1-\2', '{{cookiecutter.project_name}}').lower()
-             .sub(r'([a-z0-9])([A-Z])',r'\1-\2', '__cookiecutter.project_name__').lower()
-             .sub(r'([a-z0-9])([A-Z])',r'\1-\2', '{{cookiecutter.ns_name}}').lower()
-             .sub(r'([a-z0-9])([A-Z])',r'\1-\2', '__cookiecutter.ns_name__').lower()
